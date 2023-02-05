160a0cb4a15c6d0f9801221433f6eeebc87e5ba6
Rajeshwari <radhadhavali@gmail.com>
2023-02-05 20:42:59 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,18 @@
+class Sum 
+{
+	public static void main(String[] args) 
+	{
+		int num=568;
+		int sum=0;
+		int b=0;
+	    while(nums>0)
+		{
+			b=num%10;
+			num=num/10;
+		
+			sum=sum+b;
+		
+		}
+		System.out.println(sum);
+	}
+}
